2a8dbdff077b4e9168ac59e039e0c485a2a3047f
Serafima Dubnov <sima_dubnov@cs.huji.ac.il>
2024-07-17 10:02:19 +0300

    updated the setup.py

--- setup.py
+++ setup.py
@@ -8,14 +8,12 @@ long_description = (here / "README.md").read_text(encoding="utf-8")
 
 setup(
     name='bioib',
-    version='0.1.0',
-    packages=find_packages(where="bioIB_py"),
+    version='0.2.1',
+    packages=find_packages(),
     description="Biological Information Bottleneck",
     long_description=long_description,
     url="https://github.com/nitzanlab/bioib",
     author="Sima Dubnov",
-    # This should be a valid email address corresponding to the author listed
-    # above.
     author_email="serafima.dubnov@mail.huji.ac.il",
     classifiers = [
       "Development Status :: 5 - Production/Stable",
